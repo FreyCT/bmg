173a4013982f71f6ff0ad7dcb15acf7eeca9ad35
FreyCT <freydenit@gmail.com>
2022-01-13 12:13:15 -0800

    added images and setup index

--- client/images/index.js
+++ client/images/index.js
@@ -10,19 +10,54 @@ import image07 from './IMG_0178.jpg';
 import image08 from './IMG_0208.jpg';
 import image09 from './IMG_0360.jpeg';
 import image10 from './IMG_0410.jpeg';
-// import image11 from './';
-// import image12 from './';
-// import image13 from './';
-// import image14 from './';
-// import image15 from './';
-// import image16 from './';
-// import image17 from './';
-// import image18 from './';
-// import image19 from './';
+import image11 from './IMG_0411.jpeg';
+import image12 from './IMG_0412.jpeg';
+import image13 from './IMG_0413.jpeg';
+import image14 from './IMG_0776.jpg';
+import image15 from './IMG_2188.jpg';
+import image16 from './intersectingpop_2015.jpg';
+import image17 from './lunarcircus_2014.jpeg';
+import image18 from './mandalaA_2014.jpg';
+import image19 from './mandalaB_2014.jpg';
+import image20 from './patrik_2014.jpg';
+import image21 from './spaces1_2015.jpg';
+import image22 from './tetrix_2014.jpg';
+import image23 from './toe_2015.jpg';
+import image24 from './trans_2014.jpg';
+import image25 from './underwater_2015.jpg';
 
+const images = [
+    image00,
+    image01,
+    image02,
+    image03,
+    image04,
+    image05,
+    image06,
+    image07,
+    image08,
+    image09,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+    image21,
+    image22,
+    image23,
+    image24,
+    image25,
+]
 
 export {
 headshot,
+images,
 image00,
 image01,
 image02,
@@ -33,5 +68,20 @@ image06,
 image07,
 image08,
 image09,
-image10
+image10,
+image11,
+image12,
+image13,
+image14,
+image15,
+image16,
+image17,
+image18,
+image19,
+image20,
+image21,
+image22,
+image23,
+image24,
+image25,
 }
